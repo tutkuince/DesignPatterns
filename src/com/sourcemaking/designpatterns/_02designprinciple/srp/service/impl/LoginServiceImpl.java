5c3f8f269f93cd18256281700d80ff0dc0e7afda
tutkuince <tutku.ince@outlook.com>
2018-08-15 17:48:54 +0300

    LoginServiceImpl

---
+++ src/com/sourcemaking/designpatterns/_02designprinciple/srp/service/impl/LoginServiceImpl.java
@@ -0,0 +1,12 @@
+package com.sourcemaking.designpatterns._02designprinciple.srp.service.impl;
+
+import com.sourcemaking.designpatterns._02designprinciple.srp.model.Customer;
+import com.sourcemaking.designpatterns._02designprinciple.srp.service.LoginService;
+
+public class LoginServiceImpl implements LoginService {
+	@Override
+	public void login(Customer customer) {
+		// TODO Auto-generated method stub
+
+	}
+}
