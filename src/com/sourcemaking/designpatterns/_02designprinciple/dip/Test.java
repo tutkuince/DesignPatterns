a03665caa904c22b67316d0b6a58b3e3f8e46ee3
tutkuince <tutku.ince@outlook.com>
2018-08-15 18:31:43 +0300

    Test case for Dependency Inversion Principle

---
+++ src/com/sourcemaking/designpatterns/_02designprinciple/dip/Test.java
@@ -0,0 +1,15 @@
+package com.sourcemaking.designpatterns._02designprinciple.dip;
+
+public class Test {
+	public static void main(String[] args) {
+		Switchable switchableBulb = new LightBulb();
+		Switch bulbPowerSwitch = new ElectricPowerSwitch(switchableBulb);
+		bulbPowerSwitch.press();
+		bulbPowerSwitch.press();
+
+		Switchable switchableFan = new Fan();
+		Switch fanPowerSwitch = new ElectricPowerSwitch(switchableFan);
+		fanPowerSwitch.press();
+		fanPowerSwitch.press();
+	}
+}
