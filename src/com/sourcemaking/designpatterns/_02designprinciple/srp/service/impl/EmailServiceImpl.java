31e5437affad6f35ecfb12d0e689188cb37796d5
tutkuince <tutku.ince@outlook.com>
2018-08-15 17:48:41 +0300

    EmailService implementation

---
+++ src/com/sourcemaking/designpatterns/_02designprinciple/srp/service/impl/EmailServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sourcemaking.designpatterns._02designprinciple.srp.service.impl;
+
+import com.sourcemaking.designpatterns._02designprinciple.srp.model.Customer;
+import com.sourcemaking.designpatterns._02designprinciple.srp.service.EmailService;
+
+public class EmailServiceImpl implements EmailService {
+
+	@Override
+	public void sendEmail(Customer customer) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
