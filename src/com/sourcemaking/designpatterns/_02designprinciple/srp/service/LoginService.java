4a76c96c3ee7d6625aceeae5859894857ea189d8
tutkuince <tutku.ince@outlook.com>
2018-08-15 17:47:51 +0300

    LoginService interface

---
+++ src/com/sourcemaking/designpatterns/_02designprinciple/srp/service/LoginService.java
@@ -0,0 +1,7 @@
+package com.sourcemaking.designpatterns._02designprinciple.srp.service;
+
+import com.sourcemaking.designpatterns._02designprinciple.srp.model.Customer;
+
+public interface LoginService {
+	void login(Customer customer);
+}
