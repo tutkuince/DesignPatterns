67b741836ddd071433d9c2872738b929be93504f
tutkuince <tutku.ince@outlook.com>
2018-08-15 16:30:07 +0300

    insert

--- src/com/sourcemaking/designpatterns/_01oop/dao/impl/JdbcDAO.java
+++ src/com/sourcemaking/designpatterns/_01oop/dao/impl/JdbcDAO.java
@@ -1,5 +1,8 @@
 package com.sourcemaking.designpatterns._01oop.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 import com.sourcemaking.designpatterns._01oop.dao.DAO;
 import com.sourcemaking.designpatterns._01oop.model.Customer;
 
@@ -7,8 +10,25 @@ public class JdbcDAO implements DAO {
 
 	@Override
 	public void insert(Customer customer) {
-		// TODO Auto-generated method stub
+		Connection conn = null;
+		PreparedStatement pstmt = null;
 
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement("insert into musteri " + "(name, surname, address) values (?, ?, ?)");
+			
+			pstmt.setString(1, customer.getName());
+			pstmt.setString(2, customer.getSurname());
+			pstmt.setString(3, customer.getAddress());
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	private Connection getConnection() {
+		return null;
 	}
 
 	@Override
