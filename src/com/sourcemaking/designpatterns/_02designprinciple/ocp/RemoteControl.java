5828b939a3af9281535c38625e912239e21d8a6a
tutkuince <tutku.ince@outlook.com>
2018-08-15 17:29:33 +0300

    suitable with ocp and lc

--- src/com/sourcemaking/designpatterns/_02designprinciple/ocp/RemoteControl.java
+++ src/com/sourcemaking/designpatterns/_02designprinciple/ocp/RemoteControl.java
@@ -1,22 +1,39 @@
 package com.sourcemaking.designpatterns._02designprinciple.ocp;
 
+import com.sourcemaking.designpatterns._02designprinciple.lc.RemoteControlInterface;
+
 public class RemoteControl {
 
-	public void on(Object obj) {
+	// NOT GOOD FOR OCP
+//	public void on(Object obj) {
+//
+//		if (obj instanceof TV) {
+//			((TV) obj).tvOn();
+//		} else if (obj instanceof CDPlayer) {
+//			((CDPlayer) obj).cdPlayerOn();
+//		}
+//	}
+//	
+//	public void off(Object obj) {
+//
+//		if (obj instanceof TV) {
+//			((TV) obj).tvOff();
+//		} else if (obj instanceof CDPlayer) {
+//			((CDPlayer) obj).cdPlayerOff();
+//		}
+//	}
+	
+	private RemoteControlInterface remote;
 
-		if (obj instanceof TV) {
-			((TV) obj).tvOn();
-		} else if (obj instanceof CDPlayer) {
-			((CDPlayer) obj).cdPlayerOn();
-		}
+	public RemoteControl(RemoteControlInterface remote) {
+		this.remote = remote;
 	}
 	
-	public void off(Object obj) {
-
-		if (obj instanceof TV) {
-			((TV) obj).tvOff();
-		} else if (obj instanceof CDPlayer) {
-			((CDPlayer) obj).cdPlayerOff();
-		}
+	public void on() {
+		remote.on();
+	}
+	
+	public void off() {
+		remote.off();
 	}
 }
