51993eb9e8b019a03bcbb55f8759415b7f2a5f71
tutkuince <tutku.ince@outlook.com>
2018-08-15 17:27:06 +0300

    TV

---
+++ src/com/sourcemaking/designpatterns/_02designprinciple/ocp/TV.java
@@ -0,0 +1,12 @@
+package com.sourcemaking.designpatterns._02designprinciple.ocp;
+
+public class TV {
+	
+	public void tvOn() {
+		System.out.println("TV is opened");
+	}
+	
+	public void tvOff() {
+		System.out.println("TV is closed");
+	}
+}
