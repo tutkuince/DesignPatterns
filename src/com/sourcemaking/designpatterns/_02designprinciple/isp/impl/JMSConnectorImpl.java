abde514f03cb23e35b179e1a80199820507490a6
tutkuince <tutku.ince@outlook.com>
2018-08-15 19:43:23 +0300

    JMSConnectorImpl implementation

---
+++ src/com/sourcemaking/designpatterns/_02designprinciple/isp/impl/JMSConnectorImpl.java
@@ -0,0 +1,31 @@
+package com.sourcemaking.designpatterns._02designprinciple.isp.impl;
+
+import com.sourcemaking.designpatterns._02designprinciple.isp.JMSConnector;
+
+public class JMSConnectorImpl implements JMSConnector {
+
+	@Override
+	public void commit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void rollback() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void send(String s) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String receive() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
