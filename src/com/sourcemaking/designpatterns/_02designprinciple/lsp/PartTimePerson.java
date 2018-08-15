963504982ccc3c50d59e1c0808f68de327479551
tutkuince <tutku.ince@outlook.com>
2018-08-15 18:13:46 +0300

    PartTimePerson extends Person

---
+++ src/com/sourcemaking/designpatterns/_02designprinciple/lsp/PartTimePerson.java
@@ -0,0 +1,10 @@
+package com.sourcemaking.designpatterns._02designprinciple.lsp;
+
+public class PartTimePerson extends Person {
+	
+	@Override
+	public int salary() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
