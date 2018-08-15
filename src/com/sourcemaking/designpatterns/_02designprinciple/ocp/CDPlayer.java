c65cbc9e436f7362a7a3afac3cd3d0967656e1be
tutkuince <tutku.ince@outlook.com>
2018-08-15 17:26:52 +0300

    CDPlayer

---
+++ src/com/sourcemaking/designpatterns/_02designprinciple/ocp/CDPlayer.java
@@ -0,0 +1,12 @@
+package com.sourcemaking.designpatterns._02designprinciple.ocp;
+
+public class CDPlayer {
+	
+	public void cdPlayerOn() {
+		System.out.println("CD Player is opened");
+	}
+	
+	public void cdPlayerOff() {
+		System.out.println("CD Player is closed");
+	}
+}
