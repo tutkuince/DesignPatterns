ffa3c35d33a9aadb93af2be4545894f4c4997503
tutkuince <tutku.ince@outlook.com>
2018-08-15 18:14:00 +0300

    InternPerson

---
+++ src/com/sourcemaking/designpatterns/_02designprinciple/lsp/InternPerson.java
@@ -0,0 +1,6 @@
+package com.sourcemaking.designpatterns._02designprinciple.lsp;
+
+public class InternPerson /*extends Person*/ {
+	// This is not suitable
+	// cause, company does not pay salary to intern
+}
