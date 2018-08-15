b298515d58ae5f009fe2d1abe5160575c496f9c1
tutkuince <tutku.ince@outlook.com>
2018-08-15 17:47:11 +0300

    CustomerDAOImpl implements CUstomerDAO

---
+++ src/com/sourcemaking/designpatterns/_02designprinciple/srp/dao/impl/CustomerDaoImpl.java
@@ -0,0 +1,26 @@
+package com.sourcemaking.designpatterns._02designprinciple.srp.dao.impl;
+
+import com.sourcemaking.designpatterns._02designprinciple.srp.dao.CustomerDao;
+import com.sourcemaking.designpatterns._02designprinciple.srp.model.Customer;
+
+public class CustomerDaoImpl implements CustomerDao {
+
+	@Override
+	public void create(Customer customer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Customer customer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Customer customer) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
