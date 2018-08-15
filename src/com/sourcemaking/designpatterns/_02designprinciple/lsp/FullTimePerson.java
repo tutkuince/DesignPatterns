59d3c45369d94161f1bad9eb836ed2e14dec523e
tutkuince <tutku.ince@outlook.com>
2018-08-15 18:13:27 +0300

    FullTimePerson extends Person

---
+++ src/com/sourcemaking/designpatterns/_02designprinciple/lsp/FullTimePerson.java
@@ -0,0 +1,11 @@
+package com.sourcemaking.designpatterns._02designprinciple.lsp;
+
+public class FullTimePerson extends Person {
+
+	@Override
+	public int salary() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
