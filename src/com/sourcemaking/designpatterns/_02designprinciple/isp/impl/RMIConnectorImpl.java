978251b879e365488489c8b29d49ebe5e496d985
tutkuince <tutku.ince@outlook.com>
2018-08-15 19:43:06 +0300

    RMIConnectorImpl implementation

---
+++ src/com/sourcemaking/designpatterns/_02designprinciple/isp/impl/RMIConnectorImpl.java
@@ -0,0 +1,19 @@
+package com.sourcemaking.designpatterns._02designprinciple.isp.impl;
+
+import com.sourcemaking.designpatterns._02designprinciple.isp.RMIConnector;
+
+public class RMIConnectorImpl implements RMIConnector {
+
+	@Override
+	public void send(String s) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String receive() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
