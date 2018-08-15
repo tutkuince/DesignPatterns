4441b439dd883ecf16c43484332c704e360dc134
tutkuince <tutku.ince@outlook.com>
2018-08-15 16:02:30 +0300

    HibernateDAO impl

---
+++ src/com/sourcemaking/designpatterns/_01oop/dao/impl/HibernateDAO.java
@@ -0,0 +1,26 @@
+package com.sourcemaking.designpatterns._01oop.dao.impl;
+
+import com.sourcemaking.designpatterns._01oop.dao.DAO;
+import com.sourcemaking.designpatterns._01oop.model.Customer;
+
+public class HibernateDAO implements DAO{
+
+	@Override
+	public void insert(Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
